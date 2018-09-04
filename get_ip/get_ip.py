44443598053ad9a9721f5488854f95e3bd0b7f69
wthiel <wthiel@WTHIEL-PL.pl.oracle.com>
2018-09-04 12:53:34 +0200

    new change

--- get_ip/get_ip.py
+++ get_ip/get_ip.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     parser.add_argument('hostname', default="0.0.0.0",
                         help='The host IP to listen on. ')
 
-    parser.add_argument('port', default="8080", help='The port to listen on.')
+    parser.add_argument('port', default="8080", help='The port to listen on..')
 
     args = parser.parse_args()
 
